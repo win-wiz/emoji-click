310020503e23761ec4c17e20cfd9afae96d26e42
tjsglion <465264938@qq.com>
2024-12-03 09:18:31 +0800

    增加配置信息

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 await import("./src/env.js");
+
+if (process.env.NODE_ENV === 'development') {
+  // `await`ing the call is not necessary but it helps making sure that the setup has succeeded.
+  //  If you cannot use top level awaits you could use the following to avoid an unhandled rejection:
+  //  `setupDevPlatform().catch(e => console.error(e));`
+  await setupDevPlatform({ persist: true });
+}
 
 /** @type {import("next").NextConfig} */
 const config = {
