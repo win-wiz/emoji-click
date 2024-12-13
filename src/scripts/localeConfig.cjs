f4cd0e7611ca86bbb2d7aedc17c42e5e7f6f15b8
tjsglion <465264938@qq.com>
2024-12-13 22:27:02 +0800

    增加翻译

--- src/scripts/localeConfig.cjs
+++ src/scripts/localeConfig.cjs
@@ -1,31 +1,31 @@
 module.exports = {
   "zh": '简体中文', // 简体中文
   "en": 'English', // 英语
-  "cs": 'Čeština', // 捷克语
-  "fr": 'Français', // 法语
-  "de": 'Deutsch', // 德语
-  "es": 'Español', // 西班牙语
-  "it": 'Italiano', // 意大利语
-  "ja": '日本語', // 日语
-  "ko": '한국어', // 韩语
-  "nl": 'Nederlands', // 荷兰语
-  "pt-BR": 'Português do Brasil', // 巴西葡萄牙语
-  "ru": 'Русский', // 俄语
-  "uk": 'Українська', // 乌克兰语
-  "vi": 'Tiếng Việt', // 越南语
-  "zh-TW": '繁体中文', // 繁体中文
-  "pt": 'Português', // 葡萄牙语
-  "da": 'Dansk', // 丹麦语
-  "el": 'Ελληνικά (Elliniká)', // 希腊语
-  "no": 'Norsk', // 挪威语
-  "fi": 'Suomi', // 芬兰语
-  "sv": 'Svenska', // 瑞典语
-  "th": 'ไทย (Thai)', // 泰语
-  "id": 'Bahasa Indonesia', // 印度尼西亚语
-  "hi": 'हिन्दी (Hindi)', // 印地语
+  // "cs": 'Čeština', // 捷克语
+  // "fr": 'Français', // 法语
+  // "de": 'Deutsch', // 德语
+  // "es": 'Español', // 西班牙语
+  // "it": 'Italiano', // 意大利语
+  // "ja": '日本語', // 日语
+  // "ko": '한국어', // 韩语
+  // "nl": 'Nederlands', // 荷兰语
+  // "pt-BR": 'Português do Brasil', // 巴西葡萄牙语
+  // "ru": 'Русский', // 俄语
+  // "uk": 'Українська', // 乌克兰语
+  // "vi": 'Tiếng Việt', // 越南语
+  // "zh-TW": '繁体中文', // 繁体中文
+  // "pt": 'Português', // 葡萄牙语
+  // "da": 'Dansk', // 丹麦语
+  // "el": 'Ελληνικά (Elliniká)', // 希腊语
+  // "no": 'Norsk', // 挪威语
+  // "fi": 'Suomi', // 芬兰语
+  // "sv": 'Svenska', // 瑞典语
+  // "th": 'ไทย (Thai)', // 泰语
+  // "id": 'Bahasa Indonesia', // 印度尼西亚语
+  // "hi": 'हिन्दी (Hindi)', // 印地语
   // "ar": 'العربية (Arabic)', // 阿拉伯语 （从右往左）
   "bn": 'বাংলা (Bangla)', // 孟加拉语
-  "ms": 'Bahasa Melayu', // 马来语
-  "tr": 'Türkçe', // 土耳其语
-  "fa": 'فارسی (Farsi)', // 波斯语 （从右往左）
+  // "ms": 'Bahasa Melayu', // 马来语
+  // "tr": 'Türkçe', // 土耳其语
+  // "fa": 'فارسی (Farsi)', // 波斯语 （从右往左）
 }
--- src/scripts/openai-chat.cjs
+++ src/scripts/openai-chat.cjs
@@ -5,13 +5,13 @@ const { generateText } = require('ai');
 
 const doubao = openai.createOpenAI({
   baseURL: 'https://ark.cn-beijing.volces.com/api/v3',
-  apiKey: '22d941e5-5861-495e-bc18-600c14860f23',
+  apiKey: '892eb4d5-5b9c-4a8f-a62f-e234a171007f',
   compatibility: 'compatible'
 });
 
 console.log('apiKey===>>>>', process.env.DOUBAO_OPENAI_API_KEY);
 // 豆包模型
-const MODEL_DOUBLE_LINK_128K = 'ep-20241129111228-9bswt';
+const MODEL_DOUBLE_LINK_128K = 'ep-20241213221042-fgbmt';
 
 /**
  * @param {any} prompt
