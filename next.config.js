a78f115e0a62393eab0b3cea9b2e2d5d10a428a8
tjsglion <465264938@qq.com>
2025-01-03 17:28:47 +0800

    优化构建失败的原因

--- next.config.js
+++ next.config.js
@@ -4,14 +4,10 @@
  */
 
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-await import("./src/env.js");
+import './src/env.js';
 
 if (process.env.NODE_ENV === 'development') {
-  // `await`ing the call is not necessary but it helps making sure that the setup has succeeded.
-  //  If you cannot use top level awaits you could use the following to avoid an unhandled rejection:
-  //  `setupDevPlatform().catch(e => console.error(e));`
-  await setupDevPlatform({ persist: true });
+  setupDevPlatform({ persist: true }).catch(e => console.error(e));
 }
 
 /** @type {import("next").NextConfig} */
