f9f60f22e7b2136a54e0a03b19467a57d2168f06
tjsglion <465264938@qq.com>
2025-01-04 13:16:23 +0800

    1. 优化下拉菜单 2. 优化语言样式

--- src/components/header/language-toggle.tsx
+++ src/components/header/language-toggle.tsx
@@ -3,6 +3,7 @@
 import useI18nLocale from "@/hooks/useI18nLocale";
 import { AVAILABLE_LOCALES, DEFAULT_LOCALE } from "@/locales/config";
 import { getLocaleFlag } from "@/utils";
+import { ChevronDown } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
@@ -28,9 +29,12 @@ export default function LanguageToggle({ lang }: { lang: AVAILABLE_LOCALES }) {
         role="button"
         className="relative btn border group-hover:border-1 group-hover:border-violet-600 bg-white m-1 hover:bg-transparent"
       >
-        <p className="font-semibold flex items-center gap-2 text-md w-[122px]">
-          {getLocaleFlag(lang || DEFAULT_LOCALE)}
-          <span>{currentLang}</span>
+        <p className="font-semibold flex items-center justify-between gap-2 text-md w-[126px]">
+          <span className="ml-2">
+            {getLocaleFlag(lang || DEFAULT_LOCALE)}
+            <span className="ml-1">{currentLang}</span>
+          </span>
+          <ChevronDown className="w-4 h-4"/>
         </p>
       </div>
       <ul
