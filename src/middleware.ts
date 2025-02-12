1c117568365e81551b9b917004e1b9da8d4b34f4
tjsglion <465264938@qq.com>
2025-02-12 09:33:37 +0800

    调整文件路径

--- src/middleware.ts
+++ src/middleware.ts
@@ -44,8 +44,9 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * - .txt files
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp|txt|html)$).*)'
   ]
 }
